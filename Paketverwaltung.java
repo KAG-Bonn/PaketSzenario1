96cad7a87d59149cebd11b5ed627d71c8fc775e8
Mark Weber <73030565+nychts@users.noreply.github.com>
2022-11-25 08:13:00 +0100

    Update Paketverwaltung.java

--- Paketverwaltung.java
+++ Paketverwaltung.java
@@ -27,8 +27,8 @@ public class Paketverwaltung{
         anzahlPaketeAufStapel1 = gibAnzahlPaketeAuf(stapel1);
         anzahlPaketeAufStapel2 = gibAnzahlPaketeAuf(stapel2);
         anzahlPaketeAufStapel3 = gibAnzahlPaketeAuf(stapel3);
-        if(anzahlPaketeAufStapel1 < anzahlPaketeAufStapel2){
-            if(anzahlPaketeAufStapel1 < anzahlPaketeAufStapel3){
+        if(anzahlPaketeAufStapel1 <= anzahlPaketeAufStapel2){
+            if(anzahlPaketeAufStapel2 <= anzahlPaketeAufStapel3){
                 stapel1.push(paket);
             }
             else{
@@ -40,6 +40,12 @@ public class Paketverwaltung{
         }
         stapelAusgang.pop();
     }
+    //2.5
+    public void komplettUmstapeln(){
+        while(!stapelAusgang.isEmpty()){
+            umstapeln();
+        }
+    }
     //Methode 3
     public int gibAnzahlPaketeAuf(Stack<Paket> pStapel){
         int anzahl = 0;
@@ -49,7 +55,7 @@ public class Paketverwaltung{
             pStapel.pop();
             anzahl++;
         }
-        pStapel = tmp;
+        stapelAufStapel(pStapel, tmp);
         return anzahl;
     }
     //Methode 4
@@ -57,29 +63,53 @@ public class Paketverwaltung{
         Stack<Paket> tmp = new Stack<Paket>();
         int i = 1;
         while(!pStapel.isEmpty()){
-                tmp.push(pStapel.top());
-                System.out.println("Barcode " + i + ":" + " " + pStapel.top().gibBarcode()); 
-                i++;
-                pStapel.pop();
+            tmp.push(pStapel.top());
+            System.out.println("Barcode " + i + ":" + " " + pStapel.top().gibBarcode()); 
+            i++;
+            pStapel.pop();
         }
-        pStapel = tmp;
+        stapelAufStapel(pStapel, tmp);
     }
     //Methode 5
     public void findePaket(String pBarcode){
-        Stack<Paket>[] alleStapel = {stapel1, stapel2, stapel3};
+        Stack<Paket>[] alleStapel = new Stack[3];
+        alleStapel[0] = stapel1;
+        alleStapel[1] = stapel2;
+        alleStapel[2] = stapel3;
         
         for(int i = 0; i < alleStapel.length; i++){
             Stack<Paket> tmp = new Stack<Paket>();
-            while(!stapel1.isEmpty()){
-                if(stapel1.top().equals(pBarcode)){
-                    System.out.println("Paket mit Barcode " + pBarcode + " gefunden in Stapel " + i+1);
+            while(!alleStapel[i].isEmpty()){
+                if(alleStapel[i].top().gibBarcode().equals(pBarcode)){
+                    System.out.println("Paket mit Barcode " + pBarcode + " gefunden in Stapel " + (i+1) + "(Gewicht: " + alleStapel[i].top().gibGewicht() + ")");
+                    stapelAufStapel(alleStapel[i], tmp);
                     return;
                 }
+                tmp.push(alleStapel[i].top());
+                alleStapel[i].pop();
             }
+            stapelAufStapel(alleStapel[i], tmp);
         }
         System.out.println("Paket mit Barcode " + pBarcode + " in keinem Stapel gefunden!");
     }
-}
     
+    private void stapelAufStapel(Stack<Paket> aufStapel, Stack<Paket> vonStapel){
+        while(!vonStapel.isEmpty()){
+            aufStapel.push(vonStapel.top());
+            vonStapel.pop();
+        }
+    }
     
+    public Stack<Paket> gibStapelAusgang(){
+        return stapelAusgang;
+    }
+    public Stack<Paket> gibStapel1(){
+        return stapel1;
+    }
+    public Stack<Paket> gibStapel2(){
+        return stapel2;
+    }
+    public Stack<Paket> gibStapel3(){
+        return stapel3;
+    }
 }
